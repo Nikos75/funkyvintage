e8164671ecb5e1cc97bef14fee46cb45d3cc6c43
Nico Ouwerkerk <NIC@noldusbv.local>
2020-12-17 19:35:52 +0100

    ready for azure

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(express.static("/build/"));
+app.use(express.static(path.join(__dirname, "/build")));
 
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
+app.get("/*", (req, res) => {
+  res.sendFile("index.html", { root: __dirname + "/build" });
 });
 
 const { PORT } = process.env;
